124715f4e10ed573b3cddc42326a5088a16b1c66
damarla lasya <dlasya@gklabs.com>
2021-07-08 00:02:30 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<center style="font-size:5vw">REGISTRATION FORM</center>
+<!DOCTYPE html>
+<html>
+<style>
+body {
+  background-image: url('https://previews.123rf.com/images/wywenka/wywenka2003/wywenka200300004/141716822-a-girl-sitting-on-the-chair-works-on-the-laptop-in-social-networks-student-studying-business-woman-c.jpg');
+}
+</style>
+</body>
+</html>
---
+++ resources/views/noaccess.blade.php
@@ -0,0 +1,1 @@
+<h1>You cannot access over website</h1>
